3605c069c5084370561002a12d4010b5f6f9474a
Cristhian Gustavo Lourenço <cristhianlourenco90@gmail.com>
2025-05-10 15:41:32 -0300

    Seller entity class

--- src/main/java/com/mycompany/demo/dao/jdbc/DemoDaoJdbc.java
+++ src/main/java/com/mycompany/demo/dao/jdbc/DemoDaoJdbc.java
@@ -4,7 +4,9 @@
 
 package com.mycompany.demo.dao.jdbc;
 
+import java.util.Date;
 import model.entities.Department;
+import model.entities.Seller;
 
 /**
  *
@@ -14,6 +16,8 @@ public class DemoDaoJdbc {
 
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+        System.out.println(seller);
     }
 }
